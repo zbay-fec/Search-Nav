f6d4539959d260b29e34b5bb4d7a0c35749a19f0
tyler <tstendara@gmail.com>
2019-07-16 11:02:19 -0500

    fixed alignment of dropdown for categories and search dropdown

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@
 
 // ReactDOM.render(< App /> , document.getElementById('root'));
 import Search from './App';
+import '../dist/css/style.css';
 // import App from '@material-ui/core/App';
 
 window.Search = Search;
