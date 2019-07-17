fb3d6aed75abbccb7c773e7cf5c7ac154f026663
tyler <tstendara@gmail.com>
2019-07-17 14:58:40 -0500

    rough draft

--- client/index.js
+++ client/index.js
@@ -1,10 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
 
-// ReactDOM.render(< App /> , document.getElementById('root'));
 import Search from './App';
 import '../dist/css/style.css';
-// import App from '@material-ui/core/App';
+
 
 window.Search = Search;
