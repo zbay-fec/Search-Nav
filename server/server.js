a83ad716b57d42c42eb32d7dd7bfb887db627491
tyler <tstendara@gmail.com>
2019-07-17 20:02:51 -0500

    pm2

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 const host = process.env.HOST || "0.0.0.0";
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(parser());
+app.use(parser.json({ strict: false }));
 
 
 app.get('/items', (req, res) => {
