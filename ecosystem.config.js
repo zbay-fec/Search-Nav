844eed9153922a6b1d98f66da570291862773f70
tyler <tstendara@gmail.com>
2019-07-20 15:42:26 -0500

    deploying in production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     apps: [{
       name: 'search',
-      script: './server/server.js'
+      script: './server/server.js',
+      env: {NODE_ENV: "production"}
     }],
     deploy: {
       production: {
